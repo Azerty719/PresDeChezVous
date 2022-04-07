6fba7720a5fd2c3900f0e69fadd0021653dd6a6e
Arnaud <84045859+Anomaaaa@users.noreply.github.com>
2022-04-07 23:35:21 +0200

    Supprime fichier inutile

--- SQL/ScriptToSQL_Equipements.py
+++
@@ -1,6 +0,0 @@
-## Ce script sert à récupérer les données csv de bpe_ensemble.csv pour les transformer en commande sql. On en aura plus besoin une fois tout transformé
-
-
-#Lecture et mise en forme fichier csv
-
-with open
